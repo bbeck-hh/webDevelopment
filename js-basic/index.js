06ebe5648da82496193c9cd71c19a5963d6d5b5a
Bjoern <beck.b.hh@gmail.com>
2025-02-12 11:10:03 +0100

    feat: add initial files for JS Basics and Variables exercise, including index.js, README, and .gitignore

---
+++ js-basic/index.js
@@ -0,0 +1,23 @@
+//let firstName = 'John';
+let number = 42;
+let myMutableNumber = 100;
+
+const notMutable = 120; // Könnte ein Startpunkt für eine Berechnung sein
+
+let temp = notMutable + 5; // temp ist ein Zwischenergebnis
+
+
+console.log(`Hello firstName-> ${firstName}!`);
+console.log(`Hello number-> ${number}!`);
+console.log(`Hello myMutableNumber-> ${myMutableNumber}!`);
+console.log(`Hello const notMutable-> ${notMutable}!`);
+console.log(`Hello temp = notMutable + 5-> ${temp}!`);
+console.log(`Ergebnis von 4 * 2 ** 3-> ${4 * 2 ** 3}!`);
+// Template Literals - Nutze die Backticks ` ` am Anfang und Ende des Strings
+let firstName = 'Björn';
+console.log(`Hier kommt dein Kommentar und hier wird die Variable firstName: ${firstName} verwendet!`);
+
+
+
+
+
