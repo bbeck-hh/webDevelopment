66b18e9ee68e6dff4d918f4562a832f623131be5
Bjoern <beck.b.hh@gmail.com>
2025-02-13 15:15:55 +0100

    feat: Update weather check logic to include snowy conditions and adjust temperature handling

--- js-basic/js-conditions-and-booleans_weather-check/js/index.js
+++ js-basic/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,22 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "snowy"
+const temperature = 0;
 
-// Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+switch (weather) {
+    case "sunny":
+        // if (temperature > 20) {
+        //     console.log("It's sunny and warm! ☀️😎");
+        // } else {
+        //     console.log("It's sunny, but a bit chilly! ☀️");
+        // }
+        temperature > 20 ? console.log("It's sunny and warm! ☀️😎") : console.log("It's sunny, but a bit chilly! ☀️");
+        break;
+    case "rainy":
+        console.log("It's raining, don't forget your umbrella! ☔");
+        break;
+    case "snowy":
+        temperature <= 0 ? console.log("Freezing snow!❄️❄️") : console.log("It's snowing, stay warm! ❄️");
+        break;
+    default:
+        console.log("I'm not sure what the weather is like.");
+}
