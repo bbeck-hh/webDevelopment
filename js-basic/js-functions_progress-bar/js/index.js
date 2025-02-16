2fd9ca0691adba0f60f9f3f1bccab7e44766b119
Bjoern <beck.b.hh@gmail.com>
2025-02-16 16:39:55 +0100

    feat: Implement progress bar functionality with scroll percentage calculation and dynamic width adjustment

--- js-basic/js-functions_progress-bar/js/index.js
+++ js-basic/js-functions_progress-bar/js/index.js
@@ -2,4 +2,15 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+    // console.log(`window.scrollY: ${window.scrollY}\nwindow.innerHeight: ${window.innerHeight}\ndocument.body.clientHeight: ${document.body.clientHeight}   `);
+
+    // Berechnung des Scroll in Prozent
+    const scrollInProzent = (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100;
+    // Übergabe an den style
+    progressBar.style.width = `${scrollInProzent}%`;
+
+}
+document.addEventListener("scroll", () => {
+    calculateScrollPercentage();
+});
