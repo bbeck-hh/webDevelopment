340b4d84a6e1f1833905284cafd6f85772c1076b
Bjoern <beck.b.hh@gmail.com>
2025-02-16 16:21:39 +0100

    feat: My solotion. Refactor functions to use arrow syntax and add console output for testing

--- js-basic/js-functions_arrow-functions/js/index.js
+++ js-basic/js-functions_arrow-functions/js/index.js
@@ -21,30 +21,83 @@ function getCurrentHour() {
   }
 }
 
-function getVectorLength(x, y, z) {
+const getCurrentHourArrow = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+}
+console.log(`Hier alte Schreibweise: ${getCurrentHour()} und hier Arrow Funktion: ${getCurrentHourArrow()}  `);
+
+
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
+console.log(`Vectorlänge: ${getVectorLength(2, 3, 4).toFixed(3)} `);
 
-function cleanInput(string) {
+console.log(`
+${'*'.repeat(10)} toLowerCase ${'*'.repeat(10)}`);
+const cleanInput = (string) => {
   return string.toLowerCase().trim();
 }
+console.log(`${cleanInput("MAL Schauen, ob das alles klein geschrieben wird")} `);
 
+console.log(`
+${'*'.repeat(10)} isOddAndSmall ${'*'.repeat(10)}`);
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
+/*
 const isOddAndSmall = (number) => {
   if (number > 10) {
-    return false;
+    return 'Zahl ist kleiner 10 ? ' + false;
   }
 
   if (number % 2 === 0) {
-    return false;
+    return 'Ist eine gerade Zahl?' + false;
   }
 
-  return true;
+  return 'Beide Prüfungen vorher sind: false. Daher -> ' + true;
 };
+ */
+const isOddAndSmall = (number) => {
+  console.log(`Es wird die Zahl ${number} geprüft. Größer als 10? Gerade Zahl? `);
 
+  if (number > 10) {
+    return 'Zahl ist kleiner 10 ? ' + false;
+  }
+
+  if (number % 2 === 0) {
+    return 'Ist eine gerade Zahl? ' + false;
+  }
+
+  return 'Beide Prüfungen vorher sind: false  Daher -> ' + true;
+}
+console.log(`Ergebnis der Prüfung.: ${isOddAndSmall(7)} `);
+
+console.log(`
+${'*'.repeat(10)} add3 ${'*'.repeat(10)}`);
 const add3 = (a, b, c) => a + b + c;
+console.log(`Ergebnis: ${add3(1, 2, 5)} `);
 
+
+console.log(`
+${'*'.repeat(10)} repeat10 ${'*'.repeat(10)}`);
 const repeat10 = (string) => string.repeat(10);
+console.log(`Repeat: ${repeat10("Tee, ")} `);
+
