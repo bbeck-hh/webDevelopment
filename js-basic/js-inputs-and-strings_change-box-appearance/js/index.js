42a2399c211d1648ea5bd881cb41af637cad2069
Bjoern <beck.b.hh@gmail.com>
2025-02-17 18:49:51 +0100

    feat: Solution.  Enhance box appearance functionality with color, border radius, and rotation controls

--- js-basic/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-basic/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,33 @@
 console.clear();
+
+function getElements(datajs) {
+    return document.querySelector(`[data-js=${datajs}]`);
+}
+
+const box = getElements("box");
+const colorInput = getElements("input-color");
+const radiusInput = getElements("input-radius");
+const rotationInput = getElements("input-rotation");
+
+console.log(`${box} ${colorInput} ${radiusInput} ${rotationInput}`);
+
+colorInput.addEventListener("input", () => {
+    const hue = colorInput.value;
+    console.log(`${hue} `);
+    const colorString = `hsl(${hue}, 100%, 50%)`;
+
+    box.style.backgroundColor = colorString;
+});
+
+radiusInput.addEventListener("input", () => {
+    const radius = radiusInput.value;
+    console.log(`${radius} `);
+    box.style.borderRadius = `${radius}px`;
+});
+
+rotationInput.addEventListener("input", () => {
+    const rotation = rotationInput.value;
+    console.log(`${rotation} `);
+    box.style.transform = `rotate(${rotation}deg)`;
+});
+
