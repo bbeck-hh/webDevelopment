45bb0038d4cc7ce9a67ca6bd3e09748eebedd6a1
Bjoern <beck.b.hh@gmail.com>
2025-02-17 15:18:22 +0100

    feat: Solution. Implement input to uppercase functionality with event listener for button click

--- js-basic/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-basic/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,11 @@
 console.clear();
+const input = document.querySelector('[data-js="first-input"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener('click', () => {
+    const textToUppercase = input.value.toUpperCase();
+
+    console.log(` Nun der Text in uppercase: ${textToUppercase} `);
+
+});
+
