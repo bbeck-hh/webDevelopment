1db1a27288a365dfffa8c42659a284824a1fa2b1
Bjoern <beck.b.hh@gmail.com>
2025-02-17 08:50:15 +0100

    feat: Enhance progress bar color change based on scroll percentage thresholds

--- js-basic/js-functions_progress-bar/js/index.js
+++ js-basic/js-functions_progress-bar/js/index.js
@@ -1,5 +1,4 @@
 console.clear();
-
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
 function calculateScrollPercentage() {
@@ -8,9 +7,22 @@ function calculateScrollPercentage() {
     // Berechnung des Scroll in Prozent
     const scrollInProzent = (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100;
     // Übergabe an den style
+
+    // Farbwechsel bei 50% und 80% der width
+    //scrollInProzent > 50 ? progressBar.style.backgroundColor = "red" : progressBar.style.backgroundColor = "green";
+
+    if (scrollInProzent < 50) {
+        progressBar.style.backgroundColor = "green";
+    } else if (scrollInProzent > 50 && scrollInProzent < 80) {
+        progressBar.style.backgroundColor = "yellow"
+    } else {
+        progressBar.style.backgroundColor = "red"
+    }
+
     progressBar.style.width = `${scrollInProzent}%`;
 
 }
 document.addEventListener("scroll", () => {
     calculateScrollPercentage();
 });
+
