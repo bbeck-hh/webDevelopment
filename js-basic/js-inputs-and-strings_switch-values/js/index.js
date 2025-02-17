37c166e8c2edc82b901ecadaa97415521b43d546
Bjoern <beck.b.hh@gmail.com>
2025-02-17 16:49:51 +0100

    feat: My solution. Add second input field and switch button to toggle values between inputs

--- js-basic/js-inputs-and-strings_switch-values/js/index.js
+++ js-basic/js-inputs-and-strings_switch-values/js/index.js
@@ -1,13 +1,26 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
-const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const switchButton = document.querySelector('[data-js=button-upper-lowercase]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  // vertausche die Werte der beiden Inputs
+  const temp = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = temp;
+
+
 });
