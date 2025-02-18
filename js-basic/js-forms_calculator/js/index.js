aa71410a1d713454db7c1e6ae9f1e003aad570d7
Bjoern <beck.b.hh@gmail.com>
2025-02-18 12:51:08 +0100

    feat: Implement form submission handling for calculator operations

--- js-basic/js-forms_calculator/js/index.js
+++ js-basic/js-forms_calculator/js/index.js
@@ -3,6 +3,10 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
+
+
+
+
 function add(a, b) {
   return a + b;
 }
@@ -25,7 +29,31 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Get all the data in the form
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
 
+    switch (data.operator) {
+      case "addition":
+        resultOutput.textContent = add(Number(data.numberA), Number(data.numberB));
+        break;
+      case "subtraction":
+        resultOutput.textContent = subtract(Number(data.numberA), Number(data.numberB));
+        break;
+      case "multiplication":
+        resultOutput.textContent = multiply(Number(data.numberA), Number(data.numberB));
+        break;
+      case "division":
+        resultOutput.textContent = divide(Number(data.numberA), Number(data.numberB));
+        break;
+      default:
+        console.log(`Nichts zum rechnen da`);
+    }
+
+  });
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
