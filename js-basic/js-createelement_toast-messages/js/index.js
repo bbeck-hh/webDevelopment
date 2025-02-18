cc088f5da9ab43611d0a0f887ee7ca676dee1576
Bjoern <beck.b.hh@gmail.com>
2025-02-18 15:31:22 +0100

    feat:My solutuion.  Add functionality to append new toast messages on button click

--- js-basic/js-createelement_toast-messages/js/index.js
+++ js-basic/js-createelement_toast-messages/js/index.js
@@ -6,6 +6,10 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "Here we go";
+  toastContainer.append(newToast);
 });
 
 clearButton.addEventListener("click", () => {
