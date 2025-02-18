40ae1be5c54aa7bd8e0244d815d2299bb857c368
Bjoern <beck.b.hh@gmail.com>
2025-02-18 12:05:59 +0100

    feat: Add form submission handling and age calculation functionality

--- js-basic/js-forms_handle-form-submit/js/index.js
+++ js-basic/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+const calcAge = (age, badness) => age + badness;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    console.log(`The age-badness-sum from ${data.firstName} is ${calcAge(Number(data.age), Number(data.badness))} `);
+
+});
+
+
