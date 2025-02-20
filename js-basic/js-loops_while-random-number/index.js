acb4bb0a76027be6bd88fe23db33a206ae5a5e39
Bjoern <beck.b.hh@gmail.com>
2025-02-20 11:34:48 +0100

    feat: Solved. Implement while loop to generate random numbers until a specified limit is reached

--- js-basic/js-loops_while-random-number/index.js
+++ js-basic/js-loops_while-random-number/index.js
@@ -2,11 +2,19 @@ console.clear();
 
 let number = 0;
 let count = 0;
+const numberToStop = 12;
+console.log(`We look to beat the number: ${numberToStop}\n${'*'.repeat(30)}`);
 
 // --v-- write or modify code below this line --v--
+while (number <= numberToStop) {
+  number = Math.floor(Math.random() * 20);
+  console.log(`Number to check: ${number}`);
 
+  count++;
+}
 // --^-- write or modify code above this line --^--
 
 console.log(
-  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
+  `${'*'.repeat(30)}\nThe number that ended the loop was ${number} and it took ${count} rounds to do this!`
 );
+
