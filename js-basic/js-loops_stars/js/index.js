46a51f9973245ecfe683f46796d2e29f071ffa9c
Bjoern <beck.b.hh@gmail.com>
2025-02-20 12:55:19 +0100

    feat: Update star rendering logic to dynamically display filled and empty stars

--- js-basic/js-loops_stars/js/index.js
+++ js-basic/js-loops_stars/js/index.js
@@ -7,8 +7,22 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
+  // for (let i = 1; i < 5; i++) {
+  //   const star = document.createElement('img');
+  //   star.src = 'assets/star-empty.svg';
+  //   starContainer.appendChild(star);
+  // }
 
+  const renderStars = (filledStars) => {
+    for (let i = 0; i < 5; i++) {
+      const star = document.createElement('img');
+      star.src = i < filledStars ? 'assets/star-filled.svg' : 'assets/star-empty.svg';
+      starContainer.appendChild(star);
+    }
+  }
+
+  renderStars(2);
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+
