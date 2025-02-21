37ee5cf8ce95141dccb3278a55cbcd8c3ecf4796
Bjoern <beck.b.hh@gmail.com>
2025-02-21 19:08:28 +0100

    feat: Add Circle, Pentagon, and Square components with random color functionality on click

--- js-basic/js-structure_shapes/index.js
+++ js-basic/js-structure_shapes/index.js
@@ -1,25 +1,8 @@
 console.clear();
-import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+import Square from "./components/Square/Square.js";
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  getRandomColor();
-  circle.style.backgroundColor = getRandomColor();
-});
-
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
-
-root.append(circle, square, pentagon);
+root.append(Circle(), Square(), Pentagon());
