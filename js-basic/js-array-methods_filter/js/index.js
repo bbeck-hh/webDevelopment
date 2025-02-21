3214c6c86acdfc220d1cc22af08254a7d5daef6c
Bjoern <beck.b.hh@gmail.com>
2025-02-21 12:41:33 +0100

    feat: Solved.  Implement filter functions to retrieve specific card data based on criteria

--- js-basic/js-array-methods_filter/js/index.js
+++ js-basic/js-array-methods_filter/js/index.js
@@ -24,17 +24,28 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => {
+  return card.id === "2";
+});
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsWith3Tags = cards.filter((card) => {
+  return card.tags.length === 3;
+});
+console.log(allCardsWith3Tags);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => {
+  return card.isBookmarked === false;
+});
+console.log(allCardsThatAreNotBookmarked);
+
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  return card.isBookmarked === true && card.tags.includes("html") || card.tags.includes("js");
+});
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
-  onlyCardWithIdTwo,
-  allCardsWith3Tags,
-  allCardsThatAreNotBookmarked,
-  allCardsWithTagsHTMLOrJSThatAreBookmarked,
+  allCardsThatAreNotBookmarked, allCardsWith3Tags, allCardsWithTagsHTMLOrJSThatAreBookmarked, onlyCardWithIdTwo
 };
+
