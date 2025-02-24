d0a66a97ab6147366ed0919ae90fd5604428bafa
Bjoern <beck.b.hh@gmail.com>
2025-02-24 15:09:58 +0100

    feat:Check if this correct.  Add initial implementation of calculator with error handling for division by zero, including ESLint configuration and project setup

---
+++ js-basic/js-error-handling_calculator/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+      }
+    } catch (error) {
+      console.log(`Here in the catch block: ${error} `);
+      return "Error: Division by zero";
+    }
+
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  output.innerText = operations[operation](firstNumber, secondNumber);
+});
