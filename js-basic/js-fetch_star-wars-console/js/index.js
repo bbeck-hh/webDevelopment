1317d167c0a30f2bb81bddabbd4f6501f5bf1d34
Bjoern <beck.b.hh@gmail.com>
2025-02-24 11:00:09 +0100

    feat: Solve.  Implement async fetchData function to retrieve and log Star Wars characters

--- js-basic/js-fetch_star-wars-console/js/index.js
+++ js-basic/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,26 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.log("there was an error");
+            return;
+        }
+        const data = await response.json(); // here happens JSON.parse()
+        //console.log(data.results[0].name);
+        data.results.forEach((hero) => {
+            // All heros
+            console.log(hero.name);
+            if (hero.name === "R2-D2") {
+                console.log(`Die Augenfarbe vom Held ${hero.name} : ${hero.eye_color} `);
+            };
+        });
 
-fetchData();
+    } catch (error) {
+        console.log(`Im catch Block: ${error}`);
+    }
+}
+
+fetchData(url); 
