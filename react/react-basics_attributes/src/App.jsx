d687e011358117e86330c78d48492b71b54297ff
Bjoern <beck.b.hh@gmail.com>
2025-02-28 12:38:26 +0100

    feat: Add Article component to display a headline and input field, update App to use Article

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,17 @@
 import "./styles.css";
 
+function Article({ headline }) {
+  return (
+    <article className="article">
+      <h2 className="article__title">{headline}</h2>
+      <label htmlFor="articleInput">Eingabe: </label>
+      <input id="articleInput" type="text" />
+      <a href="https://www.perplexity.ai/?login-source=oneTapHome" className="article__link">What about a Perplexity article?</a>
+    </article>
+  )
+}
+const headline = "This is a headline";
+
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article headline={headline} />;
 }
