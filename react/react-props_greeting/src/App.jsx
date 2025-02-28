f9dc8788b9ec4fe841119c76ff4cba3c4797aa62
Bjoern <beck.b.hh@gmail.com>
2025-02-28 15:42:21 +0100

    feat: Solved. Add Greeting component to display personalized messages in App

--- react/react-props_greeting/src/App.jsx
+++ react/react-props_greeting/src/App.jsx
@@ -1,3 +1,16 @@
+function Greeting({ name }) {
+  return (
+    <h1>Hallo {name === "Pauli" ? `Coach, ${name}` : `und Moinsen, ${name}`}</h1>
+  )
+}
+
+// "Hello, Coach!" if the `name` prop matches the name of one of your coaches.
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Greeting name="Paulii" />
+      <Greeting name="Pauli" />
+    </>
+  );
 }
