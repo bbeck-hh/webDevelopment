eb8b8627868eead874bdc806cb18dc50de6b5b14
Bjoern <beck.b.hh@gmail.com>
2025-02-28 10:36:38 +0100

    feat: Add Welcome component and update App to display greeting with current year

--- react/react-basics_hello-world/src/App.jsx
+++ react/react-basics_hello-world/src/App.jsx
@@ -1,5 +1,14 @@
+import Welcome from "./components/welcome.jsx";
 import "./styles.css";
 
+const name = "World";
+const currentYear2 = new Date().getFullYear();
+
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <div>
+      Say hello...
+      <Welcome name={name} currentYear={currentYear2} />
+    </div>
+  );
 }
---
+++ react/react-basics_hello-world/src/components/welcome.jsx
@@ -0,0 +1,3 @@
+export default function Welcome({ name, currentYear }) {
+    return <h1>Hello, {name}! {currentYear}</h1>;
+}
