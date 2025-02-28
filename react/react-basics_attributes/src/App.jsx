f9a7a12af3a3ca397c8031b2669cc23dc9a03e47
Bjoern <beck.b.hh@gmail.com>
2025-02-28 15:16:34 +0100

    feat: Add Pet component to display pet details and integrate it into App

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -7,11 +7,27 @@ function Article({ headline }) {
       <label htmlFor="articleInput">Eingabe: </label>
       <input id="articleInput" type="text" />
       <a href="https://www.perplexity.ai/?login-source=oneTapHome" className="article__link">What about a Perplexity article?</a>
+      <p>Ich bin ein Paragraph</p>
     </article>
   )
 }
 const headline = "This is a headline";
 
+
+function Pet({ name, emoji, sound, isHungry, children }) {
+  return (
+    <>
+      <button>{name} {emoji} {sound} {isHungry ? "Feed me!" : sound}</button><br />
+    </>
+  );
+}
+
 export default function App() {
-  return <Article headline={headline} />;
+  return (
+    <>
+      <Article headline={headline} />
+      <Pet name="dog" emoji="🐕‍🦺" sound="Wuff" ><h2>Children prop</h2></Pet>
+      <Pet name="cat" emoji="🐈" sound="Miau" isHungry />
+    </>
+  );
 }
