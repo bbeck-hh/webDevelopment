8bf57dc0da41c6fa3f7641460ee4c73202126795
Bjoern <beck.b.hh@gmail.com>
2025-02-28 12:05:47 +0100

    feat: Add HelloWorldArticle component to display a greeting message

--- react/react-basics_hello-world-article/src/App.jsx
+++ react/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>Ich bin der Paragraph</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
