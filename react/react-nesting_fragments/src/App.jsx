0b832acf92c2f66852f9cc8a0f37356c8104a899
Bjoern <beck.b.hh@gmail.com>
2025-03-03 11:09:09 +0100

    feat: Replace Box component usage in App with new Boxes component for better structure

--- react/react-nesting_fragments/src/App.jsx
+++ react/react-nesting_fragments/src/App.jsx
@@ -1,13 +1,8 @@
-import React from "react";
+import Boxes from "./components/Boxes";
 import "./styles.css";
-import Box from "./components/Box";
 
 export default function App() {
   return (
-    <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
-    </main>
+    <Boxes />
   );
-}
+} 
