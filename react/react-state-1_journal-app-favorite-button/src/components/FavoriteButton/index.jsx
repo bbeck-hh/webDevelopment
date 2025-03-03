e174c32247de232ee168c2a9d38345dcedc21cdb
Bjoern <beck.b.hh@gmail.com>
2025-03-03 19:02:09 +0100

    feat: FavoriteButton! Implement favorite button functionality with state management

--- react/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ react/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,17 +1,23 @@
+import React, { useState } from "react";
 import "./FavoriteButton.css";
 import StarFilled from "./star-filled.svg?react";
 import Star from "./star.svg?react";
 
 export default function FavoriteButton() {
   // This should be a state variable.
-  const isFavorite = false;
+  //const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function handleClick() {
+    // Die Setter-Funktion setIsFavourite wird verwendet, um den Zustand umzuschalten
+    setIsFavorite(!isFavorite);
+    // setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+  }
 
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
+      onClick={handleClick}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
