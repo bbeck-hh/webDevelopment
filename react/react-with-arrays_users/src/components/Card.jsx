39ae1d76c19aa7709c50384a1d0ff7ce75906d35
Bjoern <beck.b.hh@gmail.com>
2025-03-04 12:11:37 +0100

    feat: Task 3 finished Enhance Tag component to conditionally highlight admin tags and update Card component to render tags

--- react/react-with-arrays_users/src/components/Card.jsx
+++ react/react-with-arrays_users/src/components/Card.jsx
@@ -8,6 +8,7 @@ function Card({ user }) {
       <h2>{user.name}</h2>
       <ul className="card__taglist">
         {user.roles.map((role, index) => {
+          // Hier wird das Tag-Element gerendert und tag={role} als prop übergeben, welche wir in der Tag.jsx dann nutzen!
           return <Tag tag={role} key={index} />;
         }
         )}
