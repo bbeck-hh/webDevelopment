9344310dc0592c023d65ea595e6e2efe35ce013d
Bjoern <beck.b.hh@gmail.com>
2025-03-04 11:31:46 +0100

    feat: Task 1 finished Update App component to render multiple user cards from USERS array

--- react/react-with-arrays_users/src/App.jsx
+++ react/react-with-arrays_users/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import "./styles.css";
 import Card from "./components/Card";
 import Title from "./components/Title";
 import { USERS } from "./db";
+import "./styles.css";
 
 export default function App() {
   return (
     <main className="app">
       <Title text="ClientBoard" />
-      <div className="app__card-grid">
+      {/* <div className="app__card-grid">
         <Card user={USERS[0]} />
-      </div>
+      </div> */}
+      {USERS.map((user) => {
+        return <Card user={user} key={user.id} />;
+      })
+      }
     </main>
   );
 }
