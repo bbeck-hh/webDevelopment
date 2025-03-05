4553d5d491d4caed2febbe5718516fe27f687c32
Bjoern <beck.b.hh@gmail.com>
2025-03-05 15:09:27 +0100

    feat: Implement loading state and fetch Pokémon data on component mount

--- react/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,28 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  async function loadPokemon() {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-      setPokemon(data.results);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        setLoading(true);
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        if (!response.ok) {
+          // API dosen't work correctly
+          throw new Error(`API antwortete nicht. Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setPokemon(data.results);
+        setLoading(false);
+      } catch (error) {
+        console.error('Sorry, there went somethig wrong. Come back later again:', error.message);
+      }
     }
-  }
+    loadPokemon(); // Call the function in useEffect
+  }, []);
 
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
-        Load Pokémon
-      </button>
-      <ul>
-        {pokemon.map(({ name }) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>We are Pokemons🤣</h1>
+          <ul>
+            {pokemon.map((p) => (
+              <li key={p.name}>{p.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </main>
   );
 }
