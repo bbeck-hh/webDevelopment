dae2c7a162b0126f530a8d36d8faf68f93a84e28
Bjoern <beck.b.hh@gmail.com>
2025-03-05 21:48:25 +0100

    feat: Implement personal details form with state management and submission handling

--- react/react-state-2_personal-details/src/App.jsx
+++ react/react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,37 @@
-import "./styles.css";
+import React, { useState } from "react";
 import Form from "./components/Form";
+import "./styles.css";
 
 export default function App() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmitDetail(details) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    // Corrosponding to the input field names
+    setName(details.name);
+    setEmail(details.email);
+    console.log("Name:", details.name);
+    console.log("Email:", details.email);
+
+    // Clear the form fields
+    event.target.reset();
+  }
+
+
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onSubmitDetail={handleSubmitDetail} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John Doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
--- react/react-state-2_personal-details/src/components/Form.jsx
+++ react/react-state-2_personal-details/src/components/Form.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
+export default function Form({ onSubmitDetail }) {
 
-export default function Form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
+    onSubmitDetail({ name, email })
   }
 
   return (
@@ -16,9 +17,20 @@ export default function Form() {
     >
       <h2 id="user-details">Please enter your details here!</h2>
       <label htmlFor="name">Name: </label>
-      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="John Doe"
+        value={name}
+        onChange={(event) => setName(event.target.value)} />
       <label htmlFor="email">Email: </label>
-      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <input id="email"
+        name="email"
+        type="email"
+        placeholder="john@doe.com"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)} />
       <button className="form__submit-button" type="submit">
         Submit
       </button>
