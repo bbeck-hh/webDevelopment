62860d686c21ce3749273f3223f7647699a54fe6
Bjoern <beck.b.hh@gmail.com>
2025-03-05 17:34:14 +0100

    feat: Enhance API status check functionality with error handling and cursor pointer on button hover

--- react/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
+++ react/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import "./StatusCheck.css";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
+  const [statusIcon, setStatusIcon] = useState("❔");
   // Something needs to change here…
   // ↙️
-  function handleCheckApiStatus() {
-    /**
-     * Hint 1:
-     * Use the `fetch()` function to send a request to `apiStatusUrl`.
-     *
-     * Hint 2:
-     * The `fetch()` function returns a promise that resolves to a Response
-     * object.
-     *
-     * Hint 3:
-     * The Response object has a `ok` property which is true if the response
-     * is okay and false if it is not.
-     **/
-    // --v-- write your code here --v--
+
+  /**
+   * Hint 1:
+   * Use the `fetch()` function to send a request to `apiStatusUrl`.
+   *
+   * Hint 2:
+   * The `fetch()` function returns a promise that resolves to a Response
+   * object.
+   *
+   * Hint 3:
+   * The Response object has a `ok` property which is true if the response
+   * is okay and false if it is not.
+   **/
+  // --v-- write your code here --v--
+  async function handleCheckApiStatus() {
+    try {
+      const response = await fetch(apiStatusUrl);
+      if (response.ok) {
+        console.log("API is up and running!");
+        setStatusIcon("✅");
+      } else {
+        setStatusIcon("❌");
+        throw new Error(`OMG -> Throw new Error: API is down! -> ${response.status}`);
+      }
+    } catch (error) {
+      console.log(`Something goes wrong, greets from the catch block: ${error.message}`);
+      setStatusIcon("❌");
+
+
+    }
     // --^-- write your code here --^--
   }
 
