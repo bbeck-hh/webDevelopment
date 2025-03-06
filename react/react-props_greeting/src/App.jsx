ce6c6b385fee1524f5802125de22f0c413f652e2
Bjoern <beck.b.hh@gmail.com>
2025-03-06 19:19:30 +0100

    feat: Enhance Greeting and Smiley components with prop usage examples and comments

--- react/react-props_greeting/src/App.jsx
+++ react/react-props_greeting/src/App.jsx
@@ -1,4 +1,8 @@
+// Props can be used to pass any type of data to components
+// Here we accept a 'name' prop that we'll use in our greeting
 function Greeting({ name }) {
+  // We can use props in conditional expressions
+  // This creates dynamic behavior based on the prop value
   return (
     <h1>Hallo {name === "Pauli" ? `Coach, ${name}` : `und Moinsen, ${name}`}</h1>
   )
@@ -9,8 +13,9 @@ function Greeting({ name }) {
 export default function App() {
   return (
     <>
-      <Greeting name="Paulii" />
-      <Greeting name="Pauli" />
+      {/* We can reuse the same component with different prop values */}
+      <Greeting name="Paulii" /> {/* Renders: Hello Paulii! */}
+      <Greeting name="Pauli" /> {/* Renders: Hello Coach Pauli! */}
     </>
   );
 }
--- react/react-props_smiley/src/App.jsx
+++ react/react-props_smiley/src/App.jsx
@@ -1,7 +1,13 @@
+// Props are like parameters that you pass to components
+// They allow components to be dynamic and reusable
 function Smiley({ isHappy }) {
+  // isHappy is a prop that we destructure from the props object
+  // When we use <Smiley isHappy={true} />, isHappy will be true
+  // When we use <Smiley /> without the prop, isHappy will be undefined (falsy)
   return (
     <>
-      <h1>Smiley 😁 component!</h1>
+      <h1>Smiley Component with Props</h1>
+      {/* We can use props to conditionally render different content */}
       <p>{isHappy ? '😁' : '😢'}</p>
     </>
   );
@@ -10,8 +16,9 @@ function Smiley({ isHappy }) {
 export default function App() {
   return (
     <>
-      <Smiley isHappy /> isHappy
-      <Smiley /> isHappy = false / nicht gesetzt
+      {/* Here we pass the isHappy prop to our Smiley component */}
+      <Smiley isHappy={true} /> {/* This will show a happy face */}
+      <Smiley /> {/* This will show a sad face since isHappy is undefined */}
     </>
   )
 }
