b78a69945a407ecaaaf06b6b75f5ef9b55e578d1
Bjoern <beck.b.hh@gmail.com>
2025-03-06 10:35:04 +0100

    feat: Add local storage handling for note and font changes, and implement delete functionality

--- react/react-with-local-storage_stored-note/src/App.jsx
+++ react/react-with-local-storage_stored-note/src/App.jsx
@@ -9,16 +9,26 @@ export default function App() {
 
   function handleNoteChange(newNote) {
     setNote(newNote);
+    localStorage.setItem("note", newNote);
   }
 
   function handleFontChange(newFont) {
     setFont(newFont);
+    localStorage.setItem("font", newFont)
+  }
+
+  function handleDeleteLocalStorage() {
+    localStorage.removeItem("note");
+    localStorage.removeItem("font");
+    setNote("");
+    setFont("system-ui");
   }
 
   return (
     <div className="app" style={{ "--font": font }}>
       <FontSelector font={font} onFontChange={handleFontChange} />
       <Note note={note} onNoteChange={handleNoteChange} />
+      <button className="btn" onClick={handleDeleteLocalStorage}>Lösche local storage</button>
     </div>
   );
 }
