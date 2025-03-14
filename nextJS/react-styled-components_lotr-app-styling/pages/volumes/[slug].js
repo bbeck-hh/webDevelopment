4843581f5bc9e20c2a281948258da1dba9332dc5
Bjoern <beck.b.hh@gmail.com>
2025-03-14 09:02:49 +0100

    feat: Refactor VolumeDetail and Volumes pages with styled components for improved layout and navigation

--- nextJS/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
+++ nextJS/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 import { volumes } from "../../lib/data";
 import ArrowLeft from "../../public/images/arrow-left.svg";
 import ArrowRight from "../../public/images/arrow-right.svg";
@@ -34,45 +34,65 @@ export default function VolumeDetail() {
 
   return (
     <>
-      {/* Setze props $bgColor={color} und übergebe color(createGlobalStyle)  */}
-      <GlobalStyle $bgColor={color} />
-
-      <Link href="/volumes"><ChevronLeft /> All Volumes</Link>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <LinkedImage href="/volumes">
-        <Image
+      <Wrapper>
+        {/* Setze props $bgColor={color} und übergebe color(createGlobalStyle)  */}
+        <GlobalStyle $bgColor={color} />
+        <BackLink href="/volumes">
+          <ChevronLeft /> All Volumes
+        </BackLink>
+        <Title>{title}</Title>
+        <Content>{description}</Content>
+        <BookList>
+          {books.map(({ ordinal, title }) => (
+            <li key={title}>
+              <SmallHeading>{ordinal}</SmallHeading>
+              <BookTitle>{title}</BookTitle>
+            </li>
+          ))}
+        </BookList>
+        <Book
           src={cover}
           alt={`Cover image of ${title}`}
           width={140}
           height={230}
         />
-
-      </LinkedImage>
-      {previousVolume ? (
-        <div>
-          <Link href={`/volumes/${previousVolume.slug}`}>
-            <ArrowLeft /> Previous Volume: {previousVolume.title}
-          </Link>
-        </div>
-      ) : null}
-      {nextVolume ? (
-        <div>
-          <Link href={`/volumes/${nextVolume.slug}`}>
-            Next Volume: {nextVolume.title} <ArrowRight />
-          </Link>
-        </div>
-      ) : null}
+        <Navigation>
+          {previousVolume ? (
+            <NavLink href={`/volumes/${previousVolume.slug}`} $position="left">
+              <ArrowLeft />
+              <div>
+                <LinkHeading>Previous Volume</LinkHeading>{" "}
+                <LinkTitle>{previousVolume.title}</LinkTitle>
+              </div>
+            </NavLink>
+          ) : null}
+          {nextVolume ? (
+            <NavLink href={`/volumes/${nextVolume.slug}`} $position="right">
+              <div>
+                <LinkHeading>Next Volume</LinkHeading>
+                <LinkTitle>{nextVolume.title}</LinkTitle>
+              </div>
+              <ArrowRight />
+            </NavLink>
+          ) : null}
+        </Navigation>
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.main`
+  padding: 1.5rem;
+`;
+
+const Title = styled.h1`
+  font: var(--font-headline-1);
+`;
+
+const Content = styled.p`
+  font: var(--font-body);
+  padding-block: 1rem;
+`;
 
 const LinkedImage = styled(Link)`
   cursor: pointer;
@@ -90,5 +110,83 @@ const LinkedImage = styled(Link)`
   > img {
     border: 12px solid red;
     width: 140px;
+  }
+`;
+
+const BackLink = styled(Link)`
+  color: unset;
+  text-decoration: none;
+  font-weight: normal;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+const BookList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+`;
+const Navigation = styled.nav`
+  padding-block: 1.5rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px;
+  grid-template-areas: "prev next";
+  align-items: start;
+`;
+
+const NavLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+  font-weight: normal;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  ${({ $position }) =>
+    $position === "left" &&
+    css`
+      grid-area: prev;
+      justify-self: start;
+      text-align: start;
+    `}
+  ${({ $position }) =>
+    $position === "right" &&
+    css`
+      grid-area: next;
+      justify-self: end;
+      text-align: end;
+    `};
+`;
+
+const LinkHeading = styled.h3`
+  font: var(--font-caption--italic);
+  margin: 0;
+`;
+
+const LinkTitle = styled.p`
+  font: var(--font-caption);
+  font-weight: bold;
+  margin: 0;
+`;
+
+const SmallHeading = styled.p`
+  font: var(--font-caption--italic);
+  margin: 0;
+  margin-bottom: 8px;
+`;
+
+const BookTitle = styled.strong`
+  font: var(--font-title);
+`;
+
+const Book = styled(Image)`
+  box-shadow: var(--box-shadow-book);
+
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
   }
 `;
--- nextJS/react-styled-components_lotr-app-styling/styles.js
+++ nextJS/react-styled-components_lotr-app-styling/styles.js
@@ -1,6 +1,6 @@
 import { Lora, Open_Sans } from "@next/font/google";
 import { createGlobalStyle } from "styled-components";
-
+// https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 const openSans = Open_Sans({
   subsets: ["latin"]
 });
@@ -15,9 +15,52 @@ export const GlobalStyle = createGlobalStyle`
   *::after {
     box-sizing: border-box;
   }
+  :root {
+  /* Color styles */
+  --color-earth: #282828;
+  --color-clouds: #f1f1f1;
+  --color-smoke: #d4d1cd;
+
+  /* Font styles */
+  --font-family: ${lora.style.fontFamily}, serif;
+  /*
+    use like:
+    font: var(--font-body);
+  */
+  --font-headline-1: normal 700 44px/48px var(--font-family);
+  --font-headline-2: normal 700 32px/41px var(--font-family);
+  --font-title: normal 700 16px/20px var(--font-family);
+  --font-caption: normal 500 12px/15px var(--font-family);
+  --font-caption--italic: italic 400 12px/15px var(--font-family);
+  --font-body: normal 400 16px/20px var(--font-family);
+
+  /* Effect styles */
+  /*
+    use like:
+    box-shadow: var(--box-shadow-book);
+  */
+  --box-shadow-book: 0 4px 8px -2px rgba(0, 0, 0, 0.09),
+    0 7px 4px -4px rgba(0, 0, 0, 0.07), 0 16px 8px -8px rgba(0, 0, 0, 0.07),
+    0 22px 12px -12px rgba(0, 0, 0, 0.07), 0 2px 10px 0 rgba(0, 0, 0, 0.06);
+
+  --box-shadow-book--hover: 0 6px 11px -2px rgba(0, 0, 0, 0.12),
+    0 8px 7px -4px rgba(0, 0, 0, 0.09), 0 17px 11px -8px rgba(0, 0, 0, 0.09),
+    0 24px 15px -12px rgba(0, 0, 0, 0.09), 0 4px 15px 0 rgba(0, 0, 0, 0.05);
+}
 
   body {
     font-family: system-ui;
     font-family: ${lora.style.fontFamily}; 
+    margin: 0;
+    font-family: var(--font-family);
+    background-color: var(--color-earth);
+    color: var(--color-smoke);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    body {
+      background-color: var(--color-clouds);
+      color: var(--color-earth)
+    }
   }
 `;
