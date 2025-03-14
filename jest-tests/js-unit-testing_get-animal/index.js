7384c5d8def00acfc411b0daa63df0566900c3db
Bjoern <beck.b.hh@gmail.com>
2025-03-14 11:19:29 +0100

    feat: Implement getAnimal function with conditional responses based on input

--- jest-tests/js-unit-testing_get-animal/index.js
+++ jest-tests/js-unit-testing_get-animal/index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+    if (animal === "dogs") {
+        return "I like dogs!";
+    }
+    if (animal === undefined) {
+        return "I do not like animals at all!";
+    }
+    return `I totally love ${animal}!`;
+}
+//getAnimal()
