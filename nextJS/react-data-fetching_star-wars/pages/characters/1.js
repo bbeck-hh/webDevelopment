f4874a4bf7a1290d308bebd8bc98e86789dc6b59
Bjoern <beck.b.hh@gmail.com>
2025-03-17 19:14:54 +0100

    feat: Implement character data fetching with SWR in Character component; update state management and rendering logic

--- nextJS/react-data-fetching_star-wars/pages/characters/1.js
+++ nextJS/react-data-fetching_star-wars/pages/characters/1.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
+import useSWR from "swr";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
 export default function Character() {
-  const id = 1;
+
+  const [id, setId] = useState(3);
+
+  const { data, error, isLoading } = useSWR(`https://swapi.py4e.com/api/people/${id}`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
